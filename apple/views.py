48b0e7d17af5e9049964046acb2129951a74d51c
Sarath-Shaji <sarathshaji2369@gmail.com>
2022-05-30 01:26:05 +0530

    automate fetching of data

--- apple/views.py
+++ apple/views.py
@@ -9,7 +9,28 @@ from django.http.response import JsonResponse
 from apple.models import Apple
 from apple.serializers import AppleSerializer
 
+import datetime
+import time
+import urllib.request
+import csv
+
 # Create your views here.
+def automatedata():
+    date_obj = datetime.datetime.now()
+    year=date_obj.year
+    month=date_obj.month
+    day=date_obj.day
+    last_year=int(datetime.datetime(year-1,month,day,0,0,0).timestamp())
+    curr_year=int(time.time())
+    data_url=f"https://query1.finance.yahoo.com/v7/finance/download/AAPL?period1={last_year}&period2={curr_year}&interval=1d&events=history&includeAdjustedClose=true"
+    urllib.request.urlretrieve(data_url,"AAPL.csv")
+    Apple.objects.all().delete()
+    with open("AAPL.csv","r") as file:
+        reader=csv.DictReader(file)
+        for row in reader:
+            data=Apple(date=row['Date'],open=row['Open'],high=row['High'],low=row['Low'],close=row['Close'],adjclose=row['Adj Close'],volume=row['Volume'])
+            data.save()
+
 @api_view(['GET'])
 def appleOverview(request):
     apple_urls={
@@ -20,12 +41,14 @@ def appleOverview(request):
 @csrf_exempt
 def appleApi(request,date="31-12-2021"):
     if request.method=='GET':
+        automatedata()
         apples = Apple.objects.filter(date=date)
         apples_serializers=AppleSerializer(apples,many=True)
         return JsonResponse(apples_serializers.data,safe=False)
 
 def getAll(request):
     if request.method=='GET':
+        automatedata()
         apples = Apple.objects.all()
         apples_serializers=AppleSerializer(apples,many=True)
         return JsonResponse(apples_serializers.data,safe=False)
